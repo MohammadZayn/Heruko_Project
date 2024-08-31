2c79f8aec91282549b59c513c51a0dfb6250644d
MohammadZayn <mohammadshaik776@gmail.com>
2024-08-31 17:32:24 +0530

    First Commit

---
+++ tests/test_no_comments.py
@@ -0,0 +1,38 @@
+import os
+import allure
+from pages.booking_page import booking
+from pages.login_page import Login
+from utils.helper import capture_screenshot
+
+
+class Test_Automation:
+    @allure.title("#Test_Case -8")
+    @allure.description("Booking the HongKong country appointment with direct date option and not "
+                        "providing any health issues")
+    @allure.tag("Integration", "#1", "TestCase")
+    @allure.label("Owner", "Mohammad_Zain")
+    @allure.testcase("Integration_Testing_8")
+    def test_Appointment(self, setup):
+        try:
+            self.lp = Login(setup)
+            self.bp = booking(setup)
+            self.lp.login_cred()
+            self.bp.facility_hongkong()
+            self.bp.healthcare_medicare()
+            self.bp.direct_date()
+            self.bp.health_issue()
+            self.bp.confirmation()
+        except Exception as e:
+            # Capture screenshot if there's an exception
+            screenshot_path = capture_screenshot(setup, name="home_page_error")
+            allure.attach.file(screenshot_path, name="Screenshot", attachment_type=allure.attachment_type.PNG)
+            raise e
+        # Capture and attach screenshot after certain actions
+        screenshot_path = capture_screenshot(setup, name="home_page_success")
+        print(f"Screenshot Path: {screenshot_path}")
+        if os.path.exists(screenshot_path):
+            print("Screenshot captured successfully.")
+        else:
+            print("Screenshot file not found.")
+        allure.attach.file(screenshot_path, name="Direct_Date_Sucess", attachment_type=allure.attachment_type.PNG)
+
